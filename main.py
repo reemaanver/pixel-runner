c4cad416659abea022ab0cdd48c55221f199adb7
reemaanver <108041797+reemaanver@users.noreply.github.com>
2024-07-08 15:08:52 +0530

    added gravity and jump feature

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ text_font = pygame.font.Font('Font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = text_font.render('My game', False, 'Black')
+
+score_surface = text_font.render('My game', False, (64, 64, 64))
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (600, 300)) 
 
 player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80, 300))
+player_gravity = 0 
  
 
 while True:
@@ -23,17 +26,32 @@ while True:
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
+        player_gravity = -20
+
+
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+        player_gravity = -20
+
   screen.blit(sky_surface, (0,0))
   screen.blit(ground_surface, (0, 300))
-  screen.blit(text_surface, (300, 50))
+  pygame.draw.rect(screen, '#c0e8ec', score_rect) 
+  pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+  screen.blit(score_surface, score_rect)
+
   snail_rect.x -= 4
   if snail_rect.right <= 0:
     snail_rect.left = 800
   screen.blit(snail_surface, snail_rect)
 
-  player_rect.x += 4
-  if player_rect.left > 800:
-    player_rect.right = 0
+  #player
+  player_gravity += 1
+  player_rect.y += player_gravity
+  if player_rect.bottom >= 300:
+    player_rect.bottom = 300
   screen.blit(player_surface, player_rect)
 
   pygame.display.update()
